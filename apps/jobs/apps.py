58e756c55e568afe81bf8b5643f2845af0fd1796
munjaehyeok <munjaehyeok@naver.com>
2026-01-18 23:48:52 +0900

    feat: 기업 즐겨찾기 기능 구현 완료

--- apps/jobs/apps.py
+++ apps/jobs/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class JobsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.jobs'
-    verbose_name = '채용 공고'
+class CorpsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField' #PK 타입설정
+    name = 'apps.jobs' 
+    verbose_name = '기업 및 채용 공고'
