7ba03ecaa7ad4486dbe2fe9b4a9f9deba0e422c0
munjaehyeok <munjaehyeok@naver.com>
2026-01-20 23:07:50 +0900

    로그인 url 수정 및 삭제 기능 구현

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -12,13 +12,14 @@ urlpatterns = [
     # 인증
     #path('signup/', views.SignupView.as_view(), name='signup'),
     #path('login/', views.LoginView.as_view(), name='login'),
-    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    #path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/logout/', views.LogoutView.as_view(), name='logout'),
-    
+    path('auth/<int:users_id>/', views.UserDeleteView.as_view(), name='user_delete'),
     # 프로필
-    path('auth/me/', views.ProfileView.as_view(), name='profile'),
+    #path('auth/me/', views.ProfileView.as_view(), name='profile'),
 
     # 구글 OAuth 인증 (Authorization Code Flow)
     path('auth/google/start/', views.GoogleLoginStartView.as_view(), name='google_start'),
     path('auth/google/callback/', views.GoogleLoginCallbackView.as_view(), name='google_callback'),
+    
 ]
