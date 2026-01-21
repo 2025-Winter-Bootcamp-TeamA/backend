56e0eff5289d5f884eb7cbe782502ae6037f7ce2
munjaehyeok <munjaehyeok@naver.com>
2026-01-21 23:34:54 +0900

    dev도커 수정

--- config/settings/local.py
+++ config/settings/local.py
@@ -30,7 +30,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # 디버그 도구
 INSTALLED_APPS += [
-    'debug_toolbar',
+    'debug_toolbar',  
+    'django_celery_beat',  # 크롤링 작업을 위해 추가
 ]
 
 MIDDLEWARE += [
@@ -46,3 +47,20 @@ INTERNAL_IPS = [
 REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] += [
     'rest_framework.renderers.BrowsableAPIRenderer',
 ]
+
+# # 1. Celery 브로커 및 결과 저장소 설정 (로컬 도커 전용)
+# # base.py의 RabbitMQ 설정을 무시하고 로컬 Redis를 사용하도록 강제합니다.
+# CELERY_BROKER_URL = 'redis://redis:6379/0'
+# CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+
+# # 2. 캐시 설정 (로컬 도커 전용)
+# # 외부 AWS 주소 접속을 차단하고 로컬 Redis 컨테이너를 바라보게 합니다.
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django_redis.cache.RedisCache',
+#         'LOCATION': 'redis://redis:6379/0',
+#         'OPTIONS': {
+#             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+#         }
+#     }
+# }
