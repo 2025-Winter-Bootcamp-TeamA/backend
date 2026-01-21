b71db9f89811db566eb358a180d42ee4d3c53370
munjaehyeok <munjaehyeok@naver.com>
2026-01-22 01:29:19 +0900

    django_celery_beat 추가

--- config/settings/base.py
+++ config/settings/base.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     # 'allauth.socialaccount',
     # 'allauth.socialaccount.providers.google',
 
+    'django_celery_beat',  # 크롤링 작업을 위해 추가
+
     # AWS S3 연동을 위한 라이브러리
     'storages',
 
